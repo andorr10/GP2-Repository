ed6b2191dec97ed6a025b4fc939004e7c3700f84
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2017-11-29 13:50:27 -0600

    incorporated pauls code with lobby.html

--- app/public/assets/js/lobby.js
+++ app/public/assets/js/lobby.js
@@ -1,11 +1,33 @@
+var player1ready = false;
+
 
 console.log("connected to lobby.js");
+console.log("Player One Status" + player1ready);
 $(document).ready(function(){
+
+
  $.get("/api/user_data").then(function(data) {
-   $(".member-email").text(data.email);
-   $(".member-username").text(data.username);
+   $(".member-email-1").text(data.email);
+   $(".member-username-1").text(data.username);
   });
-  
+
+
+  $("#citySearch").click(function(){
+    var value = $("#cityName").val();
+    console.log(value);
+    $.ajax({
+      type: "GET",
+      url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + value + "&sensor=false&key=AIzaSyDije1gHJ-JpNUMGPBbZl8Fq8c9tsu3COk",
+      dataType: "json",
+      success: processJSON
+    });
+
+    function processJSON(json) {
+      console.log(json);
+      playerReady(player1ready);
+    }
+  });
+
  function updateMove(move) {
   // console.log(move);
   $.ajax({
@@ -16,4 +38,10 @@ $(document).ready(function(){
    // console.log("finished")
   );
 }
+function playerReady(player){
+  player = true;
+  console.log("Player status " + player);
+}
+
+
 });
